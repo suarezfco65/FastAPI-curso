f9d9292e3ce58c47fee8879ff598bf5320219975
Francisco Suarez <suarezfco65@gmail.com>
2024-01-10 11:57:46 -0300

    Creando uevas rutas con parametros

--- main.py
+++ main.py
@@ -8,4 +8,13 @@ def read_root():
     return {"Hello":"Word!"}
 
 @app.get("/items/{item_id}")
-def 
+def read_item(item_id:int, q:Union[str, None] = None):
+    return {"item_id": item_id, "q":q }
+
+@app.get("/operaciones")
+def operaciones(op1:float, op2:float):
+    return {"suma": op1 + op2,
+            "diferencia": op1 - op2,
+            "producto": op1 * op2,
+            "cociente": op1 / op2}
+
